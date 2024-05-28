eb7188bb91d2d6395c3e9617540750060517b7ac
bagas aditya <bagasaditya599@gmail.com>
2024-05-29 00:54:35 +0700

    commitmen 2

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.json())
 // route utama 
 app.get('/', (req, res)=>{
-    res.send('Hello World YUtama')
+    res.send('Hello World YUtama asas')
 })
-app.get('/heloo', (req, res)=>{
+app.get('/hello', (req, res)=>{
+    console.log({urlParam: req.query})
     res.send('Hello World')
 })
+
+app.put('/username',(req, res)=>{
+    console.log({updateData: req.body})
+    res.send('update berhasil')
+})
 app.post('/login',(req,res)=>{
-    if(req.name === "bagas"){
-        res.send('login berhasil')
+    console.log({requestFromOutside: req.body})
+    const username = req.body.username
+    if(username === usernameFromDbExist){
+        res.status(400).send("username tidak dapat digunakan")
     }
+    res.send('login berhasil')
 })
 app.listen(port, ()=>{
     console.log(`Example app listening on port ${port}`)
