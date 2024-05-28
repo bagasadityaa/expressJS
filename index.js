bf4578f03e845ef33962b9edf74cfc4b3d6c3c71
bagas aditya <bagasaditya599@gmail.com>
2024-05-29 00:28:55 +0700

    commitmen 1

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-const data = "testing"
-console.log(data)
+const express = require('express')
+const app = express()
+const port = 3000
 
+// route utama 
+app.get('/', (req, res)=>{
+    res.send('Hello World YUtama')
+})
+app.get('/heloo', (req, res)=>{
+    res.send('Hello World')
+})
+app.post('/login',(req,res)=>{
+    if(req.name === "bagas"){
+        res.send('login berhasil')
+    }
+})
+app.listen(port, ()=>{
+    console.log(`Example app listening on port ${port}`)
+})
